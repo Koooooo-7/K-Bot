814f765fabbd58de9ce9b1c2e7e57ff4b725fd65
koooooo-7 <369491420@qq.com>
2020-06-15 00:11:09 +0800

    [banner]

---
+++ src/main/java/com/koy/kbot/configuration/KBotConfiguration.java
@@ -0,0 +1,25 @@
+package com.koy.kbot.configuration;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description
+ * @Auther Koy  https://github.com/Koooooo-7
+ * @Date 2020/06/06
+ */
+@Configuration
+public class KBotConfiguration {
+
+    @Bean
+    public AudioPlayerManager audioPlayerManager(){
+        DefaultAudioPlayerManager playerManager = new DefaultAudioPlayerManager();
+        AudioSourceManagers.registerRemoteSources(playerManager);
+        AudioSourceManagers.registerLocalSource(playerManager);
+        return playerManager;
+    }
+
+}
