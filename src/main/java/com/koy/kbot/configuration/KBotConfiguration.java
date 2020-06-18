15dc7803794edea4ad6e85911e57bce4e988fbd9
koooooo-7 <369491420@qq.com>
2020-06-19 00:04:48 +0800

    [bean] bean config

--- src/main/java/com/koy/kbot/configuration/KBotConfiguration.java
+++ src/main/java/com/koy/kbot/configuration/KBotConfiguration.java
@@ -1,8 +1,13 @@
 package com.koy.kbot.configuration;
 
+import com.koy.kbot.configuration.condition.ConditionalOnSummoned;
+import com.koy.kbot.listener.KBotListener;
+import com.koy.kbot.plugins.joke.Joker;
+import com.koy.kbot.plugins.time.Time;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,11 +20,25 @@ import org.springframework.context.annotation.Configuration;
 public class KBotConfiguration {
 
     @Bean
-    public AudioPlayerManager audioPlayerManager(){
+    public AudioPlayerManager audioPlayerManager() {
         DefaultAudioPlayerManager playerManager = new DefaultAudioPlayerManager();
         AudioSourceManagers.registerRemoteSources(playerManager);
         AudioSourceManagers.registerLocalSource(playerManager);
         return playerManager;
+    }
+
+    @Bean
+    @ConditionalOnClass(KBotListener.class)
+    @ConditionalOnSummoned(name = "k-bot.plugins", havingValue = "joker")
+    public Joker joker() {
+        return new Joker();
+    }
+
+    @Bean
+    @ConditionalOnClass(KBotListener.class)
+    @ConditionalOnSummoned(name = "k-bot.plugins", havingValue = "time")
+    public Time time() {
+        return new Time();
     }
 
 }
