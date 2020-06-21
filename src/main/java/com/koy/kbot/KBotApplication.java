e528aa3b2c2a3125c7bd978bab816241cf990c48
koooooo-7 <369491420@qq.com>
2020-06-22 00:20:48 +0800

    [wrapper] let JDA add listeners by collection.

--- src/main/java/com/koy/kbot/KBotApplication.java
+++ src/main/java/com/koy/kbot/KBotApplication.java
@@ -1,23 +1,26 @@
 package com.koy.kbot;
 
+import com.koy.kbot.configuration.core.JDABuilderWrapper;
 import com.koy.kbot.configuration.properties.KBotProperties;
-import com.koy.kbot.listener.KBotListener;
+import com.koy.kbot.listener.IListener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.hooks.EventListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
 import javax.security.auth.login.LoginException;
+import java.util.List;
 
 @SpringBootApplication
 public class KBotApplication {
 
     @Autowired
-    private KBotListener kBotListener;
+    private List<IListener> listeners;
 
     @Autowired
     private KBotProperties kBotProperties;
@@ -30,8 +33,8 @@ public class KBotApplication {
     // initial bot
     @PostConstruct
     public void bootstrap() throws LoginException {
-        JDA jda = JDABuilder.createDefault(kBotProperties.getToken())
-                .addEventListeners(kBotListener)
+        JDA jda = JDABuilderWrapper.createDefault(kBotProperties.getToken())
+                .addEventListeners(listeners)
                 .build();
 
         jda.getPresence().setActivity(Activity.watching("Koy Coding Show"));
---
+++ src/main/java/com/koy/kbot/configuration/core/JDABuilderWrapper.java
@@ -0,0 +1,52 @@
+package com.koy.kbot.configuration.core;
+
+import com.google.common.collect.Collections2;
+import com.koy.kbot.listener.IListener;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.hooks.EventListener;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Collection;
+
+/**
+ * @Description wrapper JDABuilder for the listeners register as list
+ * @Auther Koy  https://github.com/Koooooo-7
+ * @Date 2020/06/21
+ */
+@SuppressWarnings("deprecation")
+public class JDABuilderWrapper extends JDABuilder {
+
+    private JDABuilderWrapper(@Nullable String token, int intents)
+    {
+        this.token = token;
+        this.intents = 1 | intents;
+    }
+
+    private JDABuilderWrapper applyDefault()
+    {
+        return (JDABuilderWrapper)this.setMemberCachePolicy(MemberCachePolicy.DEFAULT)
+                .setChunkingFilter(ChunkingFilter.NONE)
+                .disableCache(CacheFlag.CLIENT_STATUS, CacheFlag.ACTIVITY)
+                .setLargeThreshold(250);
+    }
+
+    @Nonnull
+    @CheckReturnValue
+    public static JDABuilderWrapper createDefault(@Nullable String token)
+    {
+        return new JDABuilderWrapper(token, GatewayIntent.DEFAULT).applyDefault();
+    }
+
+    @Nonnull
+    public JDABuilder addEventListeners(@Nonnull Collection<IListener> listeners) {
+        net.dv8tion.jda.internal.utils.Checks.noneNull(listeners, "listeners");
+        this.listeners.addAll(listeners);
+        return this;
+    }
+}
